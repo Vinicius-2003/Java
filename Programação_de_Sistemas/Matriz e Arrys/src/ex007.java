937cdcea0d15b560503cc86790b7407afd53ecdb
Vinicius Silva Almeida <viniciusdobrazil@gmail.com.br>
2024-03-06 15:12:05 -0300

    Atualização de Exercício

--- Programação_de_Sistemas/Matriz e Arrys/src/ex007.java
+++ Programação_de_Sistemas/Matriz e Arrys/src/ex007.java
@@ -3,8 +3,8 @@ import java.util.Scanner;
 public class ex007 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int [][] matriz = new int [2][3];
-        int [][] matriz_transposta = new int [3][2];
+        int [][] matriz = new int [3][4];
+        int [][] matriz_transposta = new int [4][3];
 
         for(int i = 1; i<3;i++){
             for(int j = 1;j<4;j++){
@@ -12,9 +12,24 @@ public class ex007 {
                 matriz[i][j] = entrada.nextInt();
             }
         }
-        for(int i = 0; i<3;i++){
-            for(int j = 0;j<4;j++){
-                System.out.printf("Valor : %d",matriz[i][j]);
+        System.out.println("\nMatriz original : ");
+        for(int i = 1; i<3;i++){
+            System.out.println();
+            for(int j = 1;j<4;j++){
+                System.out.printf("%d   ",matriz[i][j]);
+            }
+        }
+        for(int i = 1; i<4;i++){
+            for(int j = 1; j<3;j++){
+                matriz_transposta[i][j] = matriz[j][i];
+            }
+        }
+        System.out.println("\n");
+        System.out.println("\nMatriz transposta : ");
+        for(int i = 1; i<4;i++){
+            System.out.println();
+            for(int j = 1;j<3;j++){
+                System.out.printf("%d   ",matriz_transposta[i][j]);
             }
         }
         entrada.close();
