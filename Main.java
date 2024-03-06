88f39f3b85ac247ffaba703e183c12afb2af3de9
Vinicius Silva Almeida <viniciusdobrazil@gmail.com.br>
2024-03-06 15:00:19 -0300

    Atividade - Finalizada

--- Main.java
+++ Main.java
@@ -1,42 +1,29 @@
 import java.util.Scanner;
 
 public class Main {
-    // public static int vetor_positivo(int[] vetor, int[] vetor_positivo) {
-    // int qtd_positivo = 0;
-    // int count = 0;
-    // for (int j = 0; j < vetor.length; j++) {
-    // if (vetor[j] >= 0) {
-    // vetor_positivo[count] = vetor[j];
-    // count++;
-    // qtd_positivo++;
-    // }
-    // }
-    // return qtd_positivo;
-    // }
-    public static void vetor_positivo(int[] vetor, int[] vetor_positivo) {
-        int qtd_positivo = 0;
-        for (int j = 0; j < vetor.length; j++) {
-            if (vetor[j] >= 0) {
-                vetor_positivo[qtd_positivo] = vetor[j];
-                qtd_positivo++;
+    public static void vetor_positivo(int[] vetor_inicial, int[] vetor_positivo) {
+        int count = 0; //Esse contador só vai funcionar quando o valor for positivo
+        for(int i = 0; i<vetor_inicial.length;i++){ //percorre o vetor inicial 
+            if(vetor_inicial[i]> 0){ //verifica se o valor é positivo
+                vetor_positivo[count] = vetor_inicial[i]; //colocar o valor positivo no vetor_positivo
+                count++; //acrescenta uma posição no contador
             }
         }
-        for (int i = 0; i < qtd_positivo;i++){
-            System.out.print(vetor_positivo);
+        System.out.println(); //pula uma linha 
+        for (int j = 0; j<count;j++){ //percorre o vetor_positivo
+            System.out.println(vetor_positivo[j]); //printa linha por linha do vetor
         }
     }
 
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
-        int[] vetor = new int[10];
+        int[] vetor_inicial = new int[10]; //cria os vetores
         int[] vetor_positivo = new int[10];
 
-        for (int i = 0; i < vetor.length; i++) {
-            vetor[i] = entrada.nextInt();
+        for (int i = 0; i < vetor_inicial.length; i++) { //Completa o vetor inicial 
+            vetor_inicial[i] = entrada.nextInt();
         }
-        vetor_positivo(vetor, vetor_positivo);
-        entrada.close();
-
+        vetor_positivo(vetor_inicial, vetor_positivo); //chama a função para realizar a operação
+        entrada.close(); //fecha o Scanner
     }
-
 }
